65b27ad5793ad0bae1f26c9a9372773dabba3dd2
Melanie Aguilar-Godinez <magui051@wch136-15.cs.ucr.edu>
2019-10-04 19:03:24 -0700

    Completed part 1

--- turnin/magui051_lab2_part1.c
+++ turnin/magui051_lab2_part1.c
@@ -15,38 +15,29 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;	
+	DDRB = 0xFF; PORTB = 0x00;	
 
 	unsigned char tmpA0 = 0x00; 
 	unsigned char tmpA1 = 0x00; 
-	unsigned char tmpA2 = 0x00; 
-	unsigned char tmpA3 = 0x00; 
-	unsigned char cntavail = 4; 
+	unsigned char tmpA2 = 0x00;
+
 	
 
     /* Insert your solution below */
     while (1) {
-	  
+	
 	tmpA0 = PINA & 0x01;
 	tmpA1 = PINA & 0x02;
-	tmpA2 = PINA & 0x04;
-	tmpA3 = PINA & 0x08;
 	
-	if (tmpA0 == 0x01) {
-		--cntavail;
+	if (tmpA0 == 0x01 && tmpA1 == 0x00) {
+		tmpA2 = (tmpA2 & 0xFE) | 0x01;
 	}
-	if (tmpA1 == 0x02) {
-		--cntavail;
+	else {
+		tmpA2 = (tmpA2 & 0xFE) | 0x00;
 	}
-	if (tmpA2 == 0x04) {
-		--cntavail;
-	}
-	if (tmpA3 == 0x08) {
-		--cntavail;
-	}
-	  
-	 PORTC = cntavail;
-	 cntavail = 4;
+	
+	PORTB = tmpA2;
+
     }
     return 1;
 }
